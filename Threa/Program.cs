d3a00ac960f153bd080a5636d412394bda2d69bf
Rockford Lhotka <rocky@lhotka.net>
2021-10-04 17:36:57 -0500

    Initial update to .NET 6

--- Threa/Program.cs
+++ Threa/Program.cs
@@ -1,26 +1,73 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Threa.Data;
+using Csla.Configuration;
+using Threa.Areas.Identity;
+using Threa.Dal.MockDb;
+using Threa.Dal.SqlServer;
+using Threa.Services;
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.AspNetCore.Components.Authorization;
+using Radzen;
 
-namespace Threa
+var builder = WebApplication.CreateBuilder(args);
+IConfiguration Configuration = null;
+IWebHostEnvironment env = null;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        Configuration.GetConnectionString("AzureThrea")));
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddTransient<SqlConnection>((a) =>
 {
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      CreateHostBuilder(args).Build().Run();
-    }
+  var db = new SqlConnection(Configuration.GetConnectionString("AzureThrea"));
+  db.Open();
+  return db;
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddSingleton<ChatHub>();
+builder.Services.AddSingleton<SessionList>();
+builder.Services.AddTransient<ChatService>();
+builder.Services.AddScoped<CircuitHandler, CircuitSessionService>();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+//services.AddMockDb();
+builder.Services.AddSqlDb();
+builder.Services.AddCsla().WithBlazorServerSupport();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
+
+Csla.ApplicationContext.LocalContext.Add("ContentRootPath", env.ContentRootPath);
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.UseCsla();
+
+app.Run();
