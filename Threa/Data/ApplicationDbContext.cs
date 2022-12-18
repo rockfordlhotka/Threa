99e57d2ec78cac881c9d3f3ab99b77f98b47e14d
Rockford Lhotka <rocky@lhotka.net>
2022-12-17 21:25:42 -0600

    Updated code to .NET 7 and fixed build issues

--- Threa/Data/ApplicationDbContext.cs
+++ Threa/Data/ApplicationDbContext.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 
-namespace Threa.Data
-{
-  public class ApplicationDbContext : IdentityDbContext
-  {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-  }
-}
+//namespace Threa.Data
+//{
+//  public class ApplicationDbContext : IdentityDbContext
+//  {
+//    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//        : base(options)
+//    {
+//    }
+//  }
+//}
