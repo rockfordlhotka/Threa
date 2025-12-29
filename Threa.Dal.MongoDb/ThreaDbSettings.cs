f4296fff84eee51963998f356efe7517805408b6
Rockford lhotka <rocky@lhotka.net>
2025-12-28 18:02:51 -0600

    Remove other dal implementations

--- Threa.Dal.MongoDb/ThreaDbSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Threa.Dal.MongoDb
-{
-  public class ThreaDbSettings
-  {
-    public string Connection { get; set; } = "mongodb://localhost:32770";
-    public string DatabaseName { get; set; } = "ThreaDb";
-  }
-}
